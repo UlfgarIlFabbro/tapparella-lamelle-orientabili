21a8785a1a88fd44b5209aa7094ea8216b7aea92
UlfgarIlFabbro <72809409+UlfgarIlFabbro@users.noreply.github.com>
2026-06-08 20:01:57 +0200

    Update __init__.py

--- custom_components/tapparella_lamelle_orientabili/__init__.py
+++ custom_components/tapparella_lamelle_orientabili/__init__.py
@@ -1,2 +1,20 @@
-async def async_setup(hass, config):
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data.setdefault(DOMAIN, {})
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "cover")
+    )
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     return True
