204af9ffb8a0bfb72004b2f2920871f977abe257
UlfgarIlFabbro <72809409+UlfgarIlFabbro@users.noreply.github.com>
2026-06-08 20:04:02 +0200

    Update cover.py

--- custom_components/tapparella_lamelle_orientabili/cover.py
+++ custom_components/tapparella_lamelle_orientabili/cover.py
@@ -3,10 +3,10 @@ from homeassistant.components.cover import CoverEntity
 
 class CherubiniCover(CoverEntity):
 
-    def __init__(self, hass, name, real_cover, ip):
+    def __init__(self, hass, name, entity_id, ip=None):
         self.hass = hass
         self._name = name
-        self._real = real_cover
+        self._entity_id = entity_id
         self._ip = ip
         self._tilt = False
 
@@ -14,29 +14,35 @@ class CherubiniCover(CoverEntity):
     def name(self):
         return self._name
 
+    @property
+    def is_closed(self):
+        return self.hass.states.is_state(self._entity_id, "closed")
+
     def open_cover(self, **kwargs):
         self.hass.services.call(
             "cover",
             "open_cover",
-            {"entity_id": self._real},
+            {"entity_id": self._entity_id},
             False
         )
         self._tilt = False
 
     def close_cover(self, **kwargs):
+        # impulso breve shelly (rest_command opzionale)
         self.hass.services.call(
-            "rest_command",
-            "tlo_close_short",
-            {},
+            "cover",
+            "close_cover",
+            {"entity_id": self._entity_id},
             False
         )
         self._tilt = False
 
     def set_cover_tilt_position(self, **kwargs):
+        # lamelle (logica motore)
         self.hass.services.call(
             "cover",
             "close_cover",
-            {"entity_id": self._real},
+            {"entity_id": self._entity_id},
             False
         )
         self._tilt = True
