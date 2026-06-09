1eaef2cb07a4cfe5a5062d8b630a0690158e85df
UlfgarIlFabbro <72809409+UlfgarIlFabbro@users.noreply.github.com>
2026-06-09 19:08:18 +0200

    const.py: sposta tutte le costanti condivise qui per evitare import circolari

--- custom_components/tapparella_lamelle_orientabili/const.py
+++ custom_components/tapparella_lamelle_orientabili/const.py
@@ -1,4 +1,14 @@
 DOMAIN = "tapparella_lamelle_orientabili"
 
-# Store globale: slug IP -> entità CherubiniCover
+STATE_OPEN = "open"
+STATE_CLOSED = "closed"
+STATE_TILT = "tilt"
+
+HA_URL = "http://192.168.1.2:8123"
+
+# Store globale: slug IP -> entita CherubiniCover
 ENTITY_STORE = {}
+
+
+def ip_slug(ip: str) -> str:
+    return ip.replace(".", "_")
