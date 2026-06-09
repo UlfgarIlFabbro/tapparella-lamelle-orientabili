932de7155bd490c9872b82d1ea83b4b889e0ec87
UlfgarIlFabbro <72809409+UlfgarIlFabbro@users.noreply.github.com>
2026-06-09 18:08:37 +0200

    cover.py: registra entita in ENTITY_STORE, rimuovi TapparellaView da qui

--- custom_components/tapparella_lamelle_orientabili/cover.py
+++ custom_components/tapparella_lamelle_orientabili/cover.py
@@ -1,10 +1,8 @@
 """Tapparella Cherubini con lamelle orientabili - controllo diretto Shelly Gen2."""
 import logging
 import aiohttp
-from aiohttp import web
 
 from homeassistant.components.cover import CoverEntity, CoverEntityFeature
-from homeassistant.components.http import HomeAssistantView
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -20,34 +18,6 @@ HA_URL = "http://192.168.1.2:8123"
 
 def _ip_slug(ip: str) -> str:
     return ip.replace(".", "_")
-
-
-class TapparellaView(HomeAssistantView):
-    """View HTTP per ricevere aggiornamenti stato dalla pressione pulsanti Shelly."""
-
-    requires_auth = False
-
-    def __init__(self, cover_entity):
-        self._cover = cover_entity
-        ip_slug = _ip_slug(cover_entity._ip)
-        self.url = f"/api/tapparella/{ip_slug}/{{action}}"
-        self.name = f"tapparella_{ip_slug}"
-
-    async def get(self, request, action):
-        cover = self._cover
-        if action == "su":
-            cover._state = STATE_OPEN
-        elif action == "giu":
-            cover._state = STATE_CLOSED
-        elif action == "lamelle":
-            cover._state = STATE_TILT
-        else:
-            return web.Response(status=404, text="Unknown action")
-
-        cover._save_state()
-        cover.async_write_ha_state()
-        _LOGGER.debug("Stato tapparella aggiornato via HTTP: %s", action)
-        return web.Response(status=200, text="OK")
 
 
 class CherubiniCover(CoverEntity):
@@ -122,6 +92,7 @@ async def async_setup_entry(
     entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
+    from . import ENTITY_STORE
     entity = CherubiniCover(hass=hass, entry=entry)
-    hass.http.register_view(TapparellaView(entity))
+    ENTITY_STORE[_ip_slug(entry.data["ip"])] = entity
     async_add_entities([entity], update_before_add=False)
