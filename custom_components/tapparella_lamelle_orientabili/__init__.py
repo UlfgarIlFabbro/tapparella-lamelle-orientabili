eac554c6e94649c8f8b8d32685564c0c709ad4af
UlfgarIlFabbro <72809409+UlfgarIlFabbro@users.noreply.github.com>
2026-06-09 08:17:51 +0200

    Fix: aggiungi async_forward_entry_setups per caricare piattaforma cover

--- custom_components/tapparella_lamelle_orientabili/__init__.py
+++ custom_components/tapparella_lamelle_orientabili/__init__.py
@@ -3,15 +3,14 @@ from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
-
 async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
+    await hass.config_entries.async_forward_entry_setups(entry, ["cover"])
     return True
 
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_unload_platforms(entry, ["cover"])
     return True
