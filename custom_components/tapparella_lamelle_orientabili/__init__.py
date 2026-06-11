110df6c8109c270f5be9dedbf3756e04f8614ee7
UlfgarIlFabbro <72809409+UlfgarIlFabbro@users.noreply.github.com>
2026-06-11 17:24:17 +0200

    Update __init__.py

--- custom_components/tapparella_lamelle_orientabili/__init__.py
+++ custom_components/tapparella_lamelle_orientabili/__init__.py
@@ -47,11 +47,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         hass.http.register_view(TapparellaView())
         _VIEW_REGISTERED = True
 
-    await hass.config_entries.async_forward_entry_setups(entry, ["cover", "sensor"])
+    await hass.config_entries.async_forward_entry_setups(entry, ["cover", "sensor", "lock"])
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     ENTITY_STORE.pop(ip_slug(entry.data.get("ip", "")), None)
-    await hass.config_entries.async_unload_platforms(entry, ["cover", "sensor"])
+    await hass.config_entries.async_unload_platforms(entry, ["cover", "sensor", "lock"])
     return True
